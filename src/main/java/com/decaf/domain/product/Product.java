77edc926aa472870f3643c7043638cd36626a3c0
leeht62 <zdfxd3611@naver.com>
2026-03-19 16:05:51 +0900

    feat: product 엔티티 추가

--- src/main/java/com/decaf/domain/product/Product.java
+++ src/main/java/com/decaf/domain/product/Product.java
@@ -1,4 +1,28 @@
 package com.decaf.domain.product;
 
-public class Product {
+import com.decaf.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product extends BaseEntity {
+
+  @Column(nullable = false, length = 100)
+  private String name;
+
+  @Column(nullable = false, length = 50)
+  private String category;
+
+  @Column(nullable = false)
+  private Integer price;
+
+  @Column(columnDefinition = "TEXT")
+  private String description;
+
 }
