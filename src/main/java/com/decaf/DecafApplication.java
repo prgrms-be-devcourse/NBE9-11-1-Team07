dca074d1bc5c26bd87f757963da0a35dd93b9849
leeht62 <zdfxd3611@naver.com>
2026-03-19 15:21:13 +0900

    docs: 패키지 경로 추가

--- src/main/java/com/decaf/DecafApplication.java
+++ src/main/java/com/decaf/DecafApplication.java
@@ -3,6 +3,7 @@ package com.decaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//경로 추가
 @SpringBootApplication
 public class DecafApplication {
 
