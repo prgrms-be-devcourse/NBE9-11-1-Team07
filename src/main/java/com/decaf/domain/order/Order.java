dffe56a3658d18eafb3f38f527c24947e3ba5c12
parkJJae <wgpark0830@naver.com>
2026-03-19 17:49:57 +0900

    featã: ì주문 엔티티 구현

--- src/main/java/com/decaf/domain/order/Order.java
+++ src/main/java/com/decaf/domain/order/Order.java
@@ -5,11 +5,23 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "orders")
 public class Order extends BaseEntity {
+
+    //User
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    //OrderItem
+//    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+//    private List<OrderItem> orderItems = new ArrayList<>();
+
 
     @Column(nullable = false)
     private String email;
