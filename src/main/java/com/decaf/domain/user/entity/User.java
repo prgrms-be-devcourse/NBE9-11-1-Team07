5896cf68f36c6a98043cfdbe7604aa44103ca20b
c1oud-dev <gksmf0187@gmail.com>
2026-03-20 11:53:54 +0900

    feat: User Entity 및 Repository 구현
    - User Entity (BaseEntity 상속)
    - UserRepository (findByEmail 메서드)

---
+++ src/main/java/com/decaf/domain/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.decaf.domain.user.entity;
+
+import com.decaf.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    // id, createDate, modifyDate는 BaseEntity에서 상속
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 200)
+    private String address;
+
+    @Column(nullable = false, length = 10)
+    private String postcode;
+
+    // TODO: Order 외래키
+    //@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    //private List<Order> orders = new ArrayList<>();
+}
