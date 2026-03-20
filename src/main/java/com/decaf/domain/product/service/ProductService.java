26ca56c367fa674d044fa96d615d49268cf72f70
parkhyunjun <lanlaria@hufs.ac.kr>
2026-03-20 10:32:29 +0900

    merge: product 브랜치 내용 반영 및 기능 통합

--- src/main/java/com/decaf/domain/product/service/ProductService.java
+++ src/main/java/com/decaf/domain/product/service/ProductService.java
@@ -34,4 +34,19 @@ public class ProductService {
     Product product = new Product(name, category, price, description);
     return productRepository.save(product);
   }
+
+    //ProductRepository를 인터페이스로 만들어서 findById와 productRepository.delete(product)를 사용가능
+    public void update(Integer id, ProductDto dto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다. id=" + id));
+
+        product.update(dto.name(), dto.price());
+    }
+
+    public void delete(Integer id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다. id=" + id));
+
+        productRepository.delete(product);
+    }
 }
