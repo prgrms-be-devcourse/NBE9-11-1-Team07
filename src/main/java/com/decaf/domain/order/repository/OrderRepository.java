9211bf3f3d4ba9eeb246c67a4d1ef6ff37cf5bc6
parkJJae <wgpark0830@naver.com>
2026-03-20 16:50:41 +0900

    feat:  주문생성 DTO에 userId 대신 email 추가 및 중복 로직 추가완료

--- src/main/java/com/decaf/domain/order/repository/OrderRepository.java
+++ src/main/java/com/decaf/domain/order/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.decaf.domain.order.repository;
 
 import com.decaf.domain.order.entity.Order;
+import com.decaf.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
 }
+
