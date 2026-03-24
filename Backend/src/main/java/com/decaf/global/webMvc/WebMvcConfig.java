ee29be5c53d336e82fcc4e49cba3bb7b7f3cd9dd
leeht62 <zdfxd3611@naver.com>
2026-03-24 10:23:14 +0900

    feature: image 등록 FileService 추가 및 Product 수정 #48

--- Backend/src/main/java/com/decaf/global/webMvc/WebMvcConfig.java
+++ Backend/src/main/java/com/decaf/global/webMvc/WebMvcConfig.java
@@ -1,18 +1,29 @@
 package com.decaf.global.webMvc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("file:" + uploadDir + "/");
     }
 }
